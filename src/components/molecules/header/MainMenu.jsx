1ebca4df027ad15361aca6b09d863257a70a8150
hansVelarde11 <hsssvf@gmail.com>
2024-04-11 21:18:00 -0500

    implementasion de iniciar y cerrar sesion

--- src/components/molecules/header/MainMenu.jsx
+++ src/components/molecules/header/MainMenu.jsx
@@ -1,18 +1,46 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
+import { deleteToken, token } from "../../../helpers/auth";
 
 const MainMenu = () => {
+
+  const nav = useNavigate()
+
+  const handleSession = () => {
+    deleteToken();
+    nav("/")
+  };
+
   return (
     <nav className="w-full">
-        <ul className="flex justify-end">
-            <li className="flex items-center">
-                <Link className="menu-item" to="/">Inicio</Link>
-            </li>
-            <li className="flex items-center">
-                <Link className="menu-item" to="/productos">Productos</Link>
-            </li>
-        </ul>
-    </nav>
-  )
-}
+      <ul className="flex justify-end">
+        <li className="flex items-center">
+          <Link className="menu-item" to="/">
+            Inicio
+          </Link>
+        </li>
+        <li className="flex items-center">
+          <Link className="menu-item" to="/productos">
+            Productos
+          </Link>
+        </li>
 
-export default MainMenu
+        {!token() ? (
+          <li className="flex items-center">
+            <Link className="menu-item" to="/login">
+              Iniciar Sesion
+            </Link>
+          </li>
+        ) : (
+          <li className="flex items-center">
+            <a onClick={handleSession} className="menu-item cursor-pointer">
+              Cerrar Sesion
+            </a>
+          </li>
+        )}
+
+      </ul>
+    </nav>
+  );
+};
+
+export default MainMenu;
